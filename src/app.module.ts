8f20b92ed95364b26fd1df01012c2e7dd6de357a
Bibek Regmi <1016matrixbibek@gmail.com>
2024-01-30 16:43:24 +0545

    database configuration added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      // host: process.env.POSTGRES_HOST,
+      // port: +process.env.PORT,
+      // username: process.env.POSTGRES_USER,
+      // password: process.env.POSTGRES_PASSWORD,
+      // database: process.env.POSTGRES_DATABASE,
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'cms_DB',
+      entities: [],
+      synchronize: true
+    }),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
