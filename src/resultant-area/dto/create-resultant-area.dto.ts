a91e2cd4a8b81b4c51679007ff1a2348bc2aa699
bibekregmi9 <vivekregmi83@gmail.com>
2024-02-11 17:32:57 +0545

    create user dto added

--- src/resultant-area/dto/create-resultant-area.dto.ts
+++ src/resultant-area/dto/create-resultant-area.dto.ts
@@ -1,3 +1,16 @@
+import { IsBoolean, IsString } from "class-validator";
+
 export class CreateResultantAreaDto {
     
+    @IsString()
+    created_by: string;
+
+    @IsString()
+    updated_by: string;
+
+    @IsString()
+    outcome: string;
+
+    @IsBoolean()
+    is_permanent: Boolean;
 }
