b46cc87f36a3b3b371fb21130fb4ae7b1b436200
bibekregmi9 <vivekregmi83@gmail.com>
2024-02-11 17:29:56 +0545

    resultant area entity completed

--- src/resultant-area/dto/create-resultant-area.dto.ts
+++ src/resultant-area/dto/create-resultant-area.dto.ts
@@ -1,1 +1,3 @@
-export class CreateResultantAreaDto {}
+export class CreateResultantAreaDto {
+    
+}
--- src/resultant-area/entities/resultant-area.entity.ts
+++ src/resultant-area/entities/resultant-area.entity.ts
@@ -1,1 +1,17 @@
-export class ResultantArea {}
+import { CommonEntity } from "src/common/common.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity({name: 'resultant_area'})
+export class ResultantArea  extends CommonEntity{
+    @Column({default: false})
+    is_permanent: Boolean;
+
+    @Column({nullable: true})
+    created_by: string;
+
+    @Column({nullable: true})
+    updated_by: string;
+
+    @Column()
+    outcome: string;
+}
